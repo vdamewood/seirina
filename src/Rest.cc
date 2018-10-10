f00b8fead1c170d82a5d377bb39585d207f387bf
Vincent Damewood <git@vdamewood.com>
2018-10-10 00:17:22 -0700

    Add Rest type

---
+++ src/Rest.cc
@@ -0,0 +1,60 @@
+/* Rest.cc: Play silence
+ *
+ * Copyright 2018 Vincent Damewood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "Rest.h"
+
+// FIXME: these shouldn't be constants here
+const int BeatLength = 18900; // 140 BPM: 44100*60/140
+
+class Rest::Pimpl
+{
+public:
+	Pimpl(double newDuration)
+		: duration(newDuration)
+	{ }
+	double duration;
+	int framePosition = 0;
+	int frameLength = 0;
+};
+
+Rest::Rest(double newDuration)
+	: p(new Pimpl(newDuration))
+{
+	p->frameLength = BeatLength * p->duration;
+}
+
+Rest::Rest(const Rest& src)
+	: p(new Pimpl(src.p->duration))
+{
+}
+
+Rest::~Rest()
+{
+	delete p;
+}
+
+AudioFrame Rest::NextFrame()
+{
+	p->framePosition++;
+	return AudioFrame(0.0, 0.0);
+}
+
+bool Rest::IsActive() const
+{
+	return p->framePosition < p->frameLength;
+
+}
---
+++ src/Rest.h
@@ -0,0 +1,38 @@
+/* Rest.h: Play silence
+ *
+ * Copyright 2018 Vincent Damewood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#if !defined REST_H
+#define REST_H
+
+#include "SonicEvent.h"
+
+class Rest : public SonicEvent
+{
+public:
+	Rest(double Duration);
+	Rest(const Rest&);
+	~Rest() override;
+
+public: // SonicEvent
+	AudioFrame NextFrame() override;
+	bool IsActive() const override;
+private:
+	class Pimpl;
+	Pimpl* p;
+};
+
+#endif // PLAYED_NOTE_H
