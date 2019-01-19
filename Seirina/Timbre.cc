6e99f266aa133a4e4d6eb0af0c05aa098545baa7
Vincent Damewood <git@vdamewood.com>
2019-01-19 05:46:40 -0800

    Make Timber use new WaveForm classes

--- Seirina/Timbre.cc
+++ Seirina/Timbre.cc
@@ -1,6 +1,6 @@
 /* Timbre.cc: Sound timbre handling
  *
- * Copyright 2016, 2018 Vincent Damewood
+ * Copyright 2016, 2018, 2019 Vincent Damewood
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,45 +21,23 @@
 
 #include "Phase.h"
 #include "SampleRate.h"
+#include "SimpleWaves.h"
 #include "Timbre.h"
 
-using Seirina::Audio::Phase;
+using Seirina::Audio::WaveForm;
+using Seirina::Audio::AbsineWave;
+using Seirina::Audio::SineWave;
+using Seirina::Audio::SquareWave;
+using Seirina::Audio::TriangleWave;
+using Seirina::Audio::SawtoothWave;
 
-typedef double (*fpWave)(Phase);
-
-static double square(Phase phase)
+static std::map<std::string, WaveForm*> Waveforms
 {
-	return phase < 0.5 ? 1.0 : -1.0;
-}
-
-static double sine(Phase phase)
-{
-	return sin(phase * M_PI * 2);
-}
-
-static double absine(Phase phase)
-{
-	return fabs(sin((phase+1.0/6.0) * M_PI)) * 2.0 - 1.0;
-}
-
-static double saw(Phase phase)
-{
-	return fmod(phase + 0.5, 1.0) * 2.0 - 1.0;
-}
-
-static double triangle(Phase phase)
-{
-	double d = fmod(phase + 0.25, 1.0);
-	return (d <= 0.5 ? d : 1.0 - d) * 4.0 - 1.0;
-}
-
-static std::map<std::string, fpWave> Waveforms
-{
-	{std::string("sine"), sine},
-	{std::string("absine"), absine},
-	{std::string("saw"), saw},
-	{std::string("square"), square},
-	{std::string("triangle"), triangle},
+	{std::string("sine"), new SineWave()},
+	{std::string("absine"), new AbsineWave()},
+	{std::string("saw"), new SawtoothWave()},
+	{std::string("square"), new SquareWave()},
+	{std::string("triangle"), new TriangleWave()},
 };
 
 double phase(double frequency, int sample)
@@ -72,22 +50,23 @@ double phase(double frequency, int sample)
 class Timbre::Pimpl
 {
 public:
-	Pimpl(fpWave NewWaveForm)
+	Pimpl(WaveForm* NewWaveForm)
+		: waveform(NewWaveForm)
 	{
-		Waveform = NewWaveForm;
 	}
 
-	fpWave Waveform;
+	WaveForm* waveform;
 };
 
 Timbre::Timbre(const char* NewWaveFormName)
+	: p(new Pimpl(Waveforms[std::string(NewWaveFormName)]))
 {
-	fpWave NewWaveForm = Waveforms[std::string(NewWaveFormName)];
-	p = new Pimpl(NewWaveForm);
+	//WaveForm* NewWaveForm = ;
+	//p = ;
 }
 
 Timbre::Timbre(const Timbre& src)
-	: p(new Pimpl(src.p->Waveform))
+	: p(new Pimpl(src.p->waveform))
 {
 
 }
@@ -100,5 +79,5 @@ Timbre::~Timbre()
 Seirina::Audio::Sample Timbre::GetSample(Note note, int sequence)
 {
 	return Seirina::Audio::Sample(
-		p->Waveform(phase(note.Pitch().Frequency(), sequence)));
+		p->waveform->GetSample(phase(note.Pitch().Frequency(), sequence)));
 }
