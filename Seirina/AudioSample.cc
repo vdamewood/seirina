0ba3c65559b549a1670f3b3b9e484dd7f290590f
Vincent Damewood <git@vdamewood.com>
2019-01-18 18:06:40 -0800

    Add AudioSample class

---
+++ Seirina/AudioSample.cc
@@ -0,0 +1,44 @@
+/* AudioSample.cc: Momentary audio amplitude
+ *
+ * Copyright 2018, 2019 Vincent Damewood
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include "AudioSample.h"
+
+inline double clip(double i)
+{
+	return
+		i < -1.0 ? 1.0
+		: i > 1.0 ? 1.0
+		: i;
+}
+
+namespace Seirina::Audio
+{
+	Sample::Sample(double newValue)
+		: value(clip(newValue))
+	{
+	}
+
+	Sample::operator double() const
+	{
+		return value;
+	}
+
+	Sample Sample::operator*(double op) const
+	{
+		return Sample(value * op);
+	}
+};
